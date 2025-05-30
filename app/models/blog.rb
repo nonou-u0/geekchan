bd3ad065860f8e1f68f84ca8a8e4ddf1a06d0402
EC2 Default User <ec2-user@ip-172-31-40-148.ap-northeast-1.compute.internal>
2025-05-30 09:36:38 +0000

    aboutページ編集

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,8 +1,8 @@
 class Blog < ApplicationRecord
   belongs_to :user
 
-  validates :title, presence: true,length: { maximum: 50}
-  validates :body, presence: true,length: { maximum: 200 }
+  validates :title, presence: true,length: { maximum: 100}
+  validates :body, presence: true,length: { maximum: 1000 }
 
   has_many :favorites, dependent: :destroy
 
